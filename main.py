24ea81cf1582e2f0fbd0722eb8e4e2799c5a90bd
Benno <bennocrafterdev@googlemail.com>
2024-07-18 15:42:08 +0200

    added file reading

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
 from src.node import Node
 
-example_text = ""
-root = Node("root", 0, {})
-# Tokenize the text
-tokenized_text = [sent.split() for sent in example_text.split(".")]
+def load_corpus(filename):
+    with open(filename, "r") as f:
+        return f.read().replace("\n", "")
 
-# Create the tree
-for sent in tokenized_text:
-    # set the current node to the root
-    current_node = root
-    for word in sent:
-        if word in current_node.children:
-            current_node = current_node.children[word]
-            current_node.count += 1
-        else:
-            new_node = Node(word, 1, {})
-            current_node.add_child(new_node)
-            current_node = new_node
-
-# Print the tree
 def print_tree(node, level=0, prefix=""):
     indent = "  " * level
     print(f"{indent}{prefix}└── {node.value} ({node.count})")
@@ -26,4 +11,26 @@ def print_tree(node, level=0, prefix=""):
         new_prefix = "├── " if i < len(node.children) - 1 else "└── "
         print_tree(child, level + 1, new_prefix)
 
-print_tree(root)
+
+
+if __name__ == "__main__":
+    example_text = load_corpus("example_corpus.txt")
+    root = Node("root", 0, {})
+    # Tokenize the text
+    tokenized_text = [sent.split() for sent in example_text.split(".")]
+
+    # Create the tree
+    for sent in tokenized_text:
+        # set the current node to the root
+        current_node = root
+        for word in sent:
+            if word in current_node.children:
+                current_node = current_node.children[word]
+                current_node.count += 1
+            else:
+                new_node = Node(word, 1, {})
+                current_node.add_child(new_node)
+                current_node = new_node
+
+    # Print the tree
+    print_tree(root)
